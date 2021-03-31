9a2cf88a966623c2941015c510511307c8f70c46
filippoomicini <filippo.omicini.stud@ispascalcomandini.it>
2021-03-31 10:29:15 +0200

    Aggiunta MetodoSomma

--- recuperoTps/recuperoTps/Program.cs
+++ recuperoTps/recuperoTps/Program.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Threading;
 
 namespace recuperoTps
 {
@@ -10,6 +11,19 @@ namespace recuperoTps
     {
         static void Main(string[] args)
         {
+            Thread t1 = new Thread(new ThreadStart(MetodoSomma));
+            
+        }
+
+        public void MetodoSomma()
+        {
+            int Add1 = 10;
+            int Add2 = 1;
+            int Somma = Add1 + Add2;
+            for (int i = 0; i < 10; i++)
+            {
+                Console.WriteLine("Somma: " + Somma);
+            }
         }
     }
 }
